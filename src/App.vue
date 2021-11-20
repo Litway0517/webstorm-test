7800286bf3566ccca1e85be65c82633c8142dab7
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-20 14:32:54 +0800

    Initial 提交 -> App主页vue文件的提交

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <useing_vue_map msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+import useing_vue_map from './views/useing_vue_map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    useing_vue_map
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
